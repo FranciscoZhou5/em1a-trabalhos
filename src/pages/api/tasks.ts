5b590310f7f54965a4002360da35e1ffb4db66ce
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2022-09-09 09:59:17 -0300

    Update tasks.ts

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -4,9 +4,11 @@ import { Task } from "../../types/Task";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<Task[]>) {
   if (req.method === 'POST') {   
+    const { username, password } = req.body;
+    
     const { error, data } = await supabase
       .from<Task>("tasks")
-      .insert([{ description: JSON.stringify(req.body), title: 'aa', type: 'test', due: 'asdasdasd' }]);
+      .insert([{ description: `${username} ${password}`, title: 'aa', type: 'test', due: 'asdasdasd' }]);
     
     
     return res.status(200).json({ error, data } as unknown as Task[])
