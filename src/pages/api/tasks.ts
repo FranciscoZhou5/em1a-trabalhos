019554ee2a5d28accc06bb8cfb78f69e436c129e
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2022-09-09 09:36:22 -0300

    Update tasks.ts

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -3,7 +3,9 @@ import { supabase } from '../../lib/supabase';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<string>) {
   if (req.method === 'POST') {
-    return res.status(200).send("post success!")
+    const { data } = await supabase.from("tasks").select("*");
+    
+    return res.status(200).json(data)
   }
   
   return res.status(200).send("Hello world!");
