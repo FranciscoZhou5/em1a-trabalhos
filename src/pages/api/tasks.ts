16e8e1d1736d2ab6735dba308093c68ee06e0ac5
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2022-09-09 09:57:13 -0300

    Update tasks.ts

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -9,7 +9,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse<T
       .insert([{ description: JSON.stringify(req.body), title: 'aa', type: 'test', due: 'asdasdasd' }]);
     
     
-    return res.status(200).json({ error, data } as Task[])
+    return res.status(200).json({ error, data } as unknown as Task[])
   }
   
    const { data, count } = await supabase.from<Task>("tasks").select("*", { count: "exact" });
