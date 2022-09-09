5e85c12838bbe643728dcf26fef8556064254076
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2022-09-09 09:28:36 -0300

    Update tasks.ts

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -1,5 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<string>) {
+  if (req.method === 'POST') {
+    return res.status(200).send("post success!")
+  }
+  
   return res.status(200).send("Hello world!");
 }
