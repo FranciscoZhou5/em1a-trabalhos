edb05060866bc6da6a34f5cde2f3e54f15499c85
Francisco Zhou <113115839+FranciscoZhou5@users.noreply.github.com>
2022-09-09 09:55:37 -0300

    Update tasks.ts

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -3,15 +3,10 @@ import { supabase } from '../../lib/supabase';
 import { Task } from "../../types/Task";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<Task[]>) {
-  if (req.method === 'POST') {
-    
-    setTimeout(() => {
-    
-      const { error, data } = await supabase
-        .from<Task>("tasks")
-        .insert([{ description: JSON.stringify(req.body), title: 'aa', type: 'test', due: 'asdasdasd' }]);
-    
-    }, 2000)
+  if (req.method === 'POST') {   
+    const { error, data } = await supabase
+      .from<Task>("tasks")
+      .insert([{ description: JSON.stringify(req.body), title: 'aa', type: 'test', due: 'asdasdasd' }]);
     
     
     return res.status(200).json({ error, data })
