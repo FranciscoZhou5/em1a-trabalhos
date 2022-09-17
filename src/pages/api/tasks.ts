5d94198ffce3c6d8e00b22d5b34c110043c1c68e
FranciscoZhou5 <fg45874587@proton.me>
2022-09-17 13:25:48 -0300

    Change tasks route

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -1,20 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from '../../lib/supabase';
+import { supabase } from "../../lib/supabase";
 import { Task } from "../../types/Task";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse<Task[]>) {
-  if (req.method === 'POST') {   
-    const { username, password } = req.body;
-    
-    const { error, data } = await supabase
-      .from<Task>("tasks")
-      .insert([{ description: `${username} ${password}`, title: 'aa', type: 'test', due: "09/09/2022" }]);
-    
-    
-    return res.status(200).json({ error, data } as unknown as Task[])
-  }
-  
-   const { data, count } = await supabase.from<Task>("tasks").select("*", { count: "exact" });
-  
+  // if (req.method === 'POST') {
+  //   const { username, password } = req.body;
+
+  //   const { error, data } = await supabase
+  //     .from<Task>("tasks")
+  //     .insert([{ description: `${username} ${password}`, title: 'aa', type: 'test', due: "09/09/2022" }]);
+
+  //   return res.status(200).json({ error, data } as unknown as Task[])
+  // }
+
+  const { data, count } = await supabase.from<Task>("tasks").select("*", { count: "exact" });
+
   return res.status(200).send(data as Task[]);
 }
